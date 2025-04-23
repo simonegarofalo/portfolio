b845700e2e14b31e84b2f6994a5df1802af81198
simonegarofalo <simonegarofalo96@gmail.com>
2025-04-23 15:37:53 +0200

    Updated email text

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -210,7 +210,7 @@ const mailjet = Mailjet.apiConnect(
       <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#218DF1;background-color:#218DF1;width:100%;">
         <tbody>
           <tr>
-            <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;">
+            <td style="direction:ltr;font-size:0px;padding:20px 0 0 0;text-align:center;">
               <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
               <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
                 <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
